8c56e8bab86de3a2355832a3c74a4b8783a9ece7
rustutam <Rustam.Asbapov@urfu.me>
2023-05-29 15:26:53 +0500

    Исправлен вывод

--- main.py
+++ main.py
@@ -4,29 +4,27 @@ import re
 
 
 def print_friends(string):
-    pattern = r">(\w+)\s(\w+)<"
-    friends = re.findall(pattern, string)
     len_s = 25
     print("-" * len_s)
-    for human in friends:
-        first_name = human[1]
-        last_name = human[0]
+    for human in string:
+        first_name = human[0]
+        last_name = human[1]
         print(f"First Name: {first_name}\n"
-              f"Last Name: {last_name}")
+              f" Last Name: {last_name}")
         print("-" * len_s)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-uid", required=True)
-    parser.add_argument("-aid", required=True, help="id of vk application")
-    parser.add_argument("-tkn", required=True,
+    parser.add_argument("-userid", required=True)
+    parser.add_argument("-appid", required=True, help="id of vk application")
+    parser.add_argument("-token", required=True,
                         help="access_token of vk_app")
     cnsl_args = parser.parse_args().__dict__
-    protocols_app = VK_App(cnsl_args["aid"], cnsl_args["tkn"])
-    user_id = cnsl_args["uid"]
-    rep = protocols_app.get_friends_list(user_id)['items']
-    res = "\n".join(["<p>" + f'<a href="https://vk.com/id{repl["id"]}">'
-                     + repl['last_name'] + " " + repl['first_name']
-                     + "</a>" + "</p>" for repl in rep])
+    protocols_app = VK_App(cnsl_args["appid"], cnsl_args["token"])
+    user_id = cnsl_args["userid"]
+    humans_dict = protocols_app.get_friends_list(user_id)['items']
+    res = []
+    for human in humans_dict:
+        res.append([human['first_name'], human['last_name']])
     print_friends(res)
