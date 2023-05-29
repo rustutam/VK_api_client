2a9db2b81d99991cfd53ba53446bdaea3c47cfb0
rustutam <Rustam.Asbapov@urfu.me>
2023-05-29 11:12:15 +0500

    Готовая версия Vk_api для вывода списка друзей указанного пользователя

---
+++ main.py
@@ -0,0 +1,34 @@
+from vk_app import VK_App
+import argparse
+import re
+
+
+def print_friends(string):
+    pattern = r">(\w+)\s(\w+)<"
+    friends = re.findall(pattern, string)
+    print(friends)
+    len_s = 25
+    print("-" * len_s)
+    for human in friends:
+        first_name = human[1]
+        last_name = human[0]
+        print(f"First Name: {first_name}\n"
+              f"Last Name: {last_name}")
+        print("-" * len_s)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-uid", required=True,
+                        help="id or domain screen name of user\nexample: -id 122344 OR -id id56567 OR -id pornoiya")
+    parser.add_argument("-aid", required=True, help="id of vk application")
+    parser.add_argument("-tkn", required=True,
+                        help="access token of vk application")
+    cnsl_args = parser.parse_args().__dict__
+    protocols_app = VK_App(cnsl_args["aid"], cnsl_args["tkn"])
+    user_id = cnsl_args["uid"]
+    rep = protocols_app.get_friends_list(user_id)['items']
+    res = "\n".join(["<p>" + f'<a href="https://vk.com/id{repl["id"]}">'
+                     + repl['last_name'] + " " + repl['first_name']
+                     + "</a>" + "</p>" for repl in rep])
+    print_friends(res)
---
+++ vk_app.py
@@ -0,0 +1,36 @@
+from urllib.request import Request, urlopen
+import json
+
+
+class VK_App:
+    def __init__(self, id, token):
+        self.id = id
+        self.token = token
+        self.api_version = "5.131"
+
+    @staticmethod
+    def make_request(req_message):
+        req_obj = Request(req_message,
+                          headers={'User-Agent': 'Mozilla/5.0'})
+        page = urlopen(req_obj)
+        return json.loads(page.read())
+
+    def id_by_screen_name(self, screen_name):
+        id_by_screenname_req = f"https://api.vk.com/method/users.get?user_ids={screen_name}" \
+            f"&access_token={self.token}&v={self.api_version}"
+        response = VK_App.make_request(id_by_screenname_req)
+        return response["response"][0]["id"]
+
+    def get_friends_list(self, user_id):
+        if not user_id.isdigit():
+            try:
+                user_id = self.id_by_screen_name(user_id)
+            except KeyError:
+                raise ValueError("user does not exist")
+        friends_list_req = f"https://api.vk.com/method/friends.get?user_id={user_id}" \
+            f"&order=hints&fields=nickname,domain&access_token={self.token}&v={self.api_version}"
+        try:
+            return self.make_request(friends_list_req)['response']
+        except KeyError:
+            raise KeyError(f"{self.make_request(friends_list_req)['error']['error_msg']}")
+
